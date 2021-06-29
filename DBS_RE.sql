71ce98c25ecb9e5af08b61105212b821d5f9711d
Agam Singh <79820189+Agam-S@users.noreply.github.com>
2021-06-29 11:26:07 +1000

    Task 3 done

--- DBS_RE.sql
+++ DBS_RE.sql
@@ -45,7 +45,7 @@ IF OBJECT_ID('TEACHER') IS NOT NULL
 GO
 
 CREATE TABLE TEACHER(
-    StaffID INT CHECK (StaffID = 8),
+    StaffID INT CHECK (LEN(StaffID) = 8),
     Surname NVARCHAR(100) NOT NULL,
     GivenName NVARCHAR(100) NOT NULL,
 
@@ -72,7 +72,7 @@ GO
 
 CREATE TABLE SUBJECTOFFERING(
     SubjCode NVARCHAR(100),
-    Year INT CHECK (Year = 4),
+    Year INT CHECK (LEN(Year) = 4),
     Semester INT,
     CONSTRAINT CHK_Semester CHECK (Semester IN (1, 2)),
     Fee MONEY NOT NULL CHECK(Fee > 0),
@@ -90,7 +90,7 @@ GO
 CREATE TABLE ENROLLMENT(
     StudentID NVARCHAR(10),
     SubjCode NVARCHAR(100),
-    Year INT CHECK (Year = 4),
+    Year INT CHECK (LEN(Year) = 4),
     Semester INT,
     CONSTRAINT CHK_Semester_Enrollment CHECK (Semester IN (1, 2)),
     Grade NVARCHAR(2),
@@ -103,6 +103,44 @@ CREATE TABLE ENROLLMENT(
 );
 
 
-
 select table_name from information_schema.tables;
 exec sp_columns project;
+
+
+INSERT INTO SUBJECT(SubjCode, Description) VALUES
+('ICTPRG418', 'Apply SQL to extract & manipulate data'),
+('ICTBSB430', 'Create Basic Databases'),
+('ICTDBS205', 'Design a Database');
+
+INSERT INTO STUDENT(StudentID, Surname, GivenName, Gender) VALUES
+('s12233445', 'Baird', 'Tim', 'M'),
+('s23344556', 'Nguyen', 'Anh', 'M'),
+('s34455667', 'Hallinan', 'James', 'M'),
+('s45455669', 'Singh', 'Agampreet', 'M');
+
+INSERT INTO TEACHER(StaffID, Surname, GivenName) VALUES
+(98776655, 'Young', 'Angus'),
+(87665544, 'Scott', 'Bon'),
+(76554433, 'Slade', 'Chris');
+
+INSERT INTO SUBJECTOFFERING(SubjCode, Year, Semester, Fee, StaffID) VALUES
+('ICTPRG418', 2019, 1, 200, 98776655),
+('ICTPRG418', 2020, 1, 225, 98776655),
+('ICTBSB430', 2020, 1, 200, 87665544),
+('ICTBSB430', 2020, 2, 200, 76554433),
+('ICTDBS205', 2019, 2, 225, 87665544);
+
+INSERT INTO ENROLLMENT(StudentID, SubjCode, Year, Semester, Grade, DateEnrolled) VALUES
+('s12233445', 'ICTPRG418', 2019, 1, 'D', '2/25/2019'),
+('s23344556', 'ICTPRG418', 2019, 1, 'P', '2/15/2019'),
+('s12233445', 'ICTPRG418', 2020, 1, 'C', '1/30/2020'),
+('s23344556', 'ICTPRG418', 2020, 1, 'HD', '2/26/2020'),
+('s34455667', 'ICTPRG418', 2020, 1, 'P', '1/28/2020'),
+('s12233445', 'ICTBSB430', 2020, 1, 'C', '2/08/2020'),
+('s23344556', 'ICTBSB430', 2020, 2, NULL, '6/30/2020'),
+('s34455667', 'ICTBSB430', 2020, 2, NULL, '7/03/2020'),
+('s23344556', 'ICTDBS205', 2019, 2, 'P', '7/01/2019'),
+('s34455667', 'ICTDBS205', 2019, 2, 'N', '7/13/2019'),
+
+('s45455669', 'ICTPRG418', 2020, 1, 'HD', '5/03/2020');
+
